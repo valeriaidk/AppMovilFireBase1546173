727bcc86603a3d18370b53a2e8ef11544471738f
valeriaidk <valeriaquezada6@hotmail.com>
2025-02-21 19:24:50 -0500

    inicio y menu 21/02

--- app/src/main/java/com/example/appmovil1546173/MainActivity.java
+++ app/src/main/java/com/example/appmovil1546173/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.appmovil1546173;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //INICIAR OBJETOS PARA CADA EN XML  -  JAVA  - XML
+        Button btnLogin = findViewById(R.id.btnLogin);
+        TextView lblregistrate = findViewById(R.id.lblRegistrate);
+        TextView lblresetpass = findViewById(R.id.lblResetPass);
+        EditText txtEmail = findViewById(R.id.txtEmailLogin);
+        EditText txtPassLogin = findViewById(R.id.txtPassLogin) ;
+
+        //TEXTVIEW lblregistrate debe abrir view -> registrate
+
+        lblregistrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent conectar = new Intent(MainActivity.this,Register.class);
+                startActivity(conectar);
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent conectar = new Intent(MainActivity.this,menu.class);
+                startActivity(conectar);
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/appmovil1546173/inicio.java
+++ app/src/main/java/com/example/appmovil1546173/inicio.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,37 +11,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Register extends AppCompatActivity {
+public class inicio extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_inicio);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
-
         });
 
-        Button btnCancelarRegister = findViewById(R.id.btnCancelarRegister);
-        Button btnRegistrar = findViewById(R.id.btnRegistrar);
-        Button btnlimpiar = findViewById(R.id.btnLimpiar);
+        Button btnLoginInicio = findViewById(R.id.btnLoginInicio);
 
-        EditText txtApellidoNombre = findViewById(R.id.txtApellidoNombre);
-        EditText textCorreoElectronico = findViewById(R.id.txtCorreoElectronico);
-        EditText txtTelefono = findViewById(R.id.txtTelefono);
-        EditText txtContraseña = findViewById(R.id.txtContraseña);
-
-        btnCancelarRegister.setOnClickListener(new View.OnClickListener() {
+        btnLoginInicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent conec = new Intent(Register.this,MainActivity.class);
+                Intent conec = new Intent(inicio.this,MainActivity.class);
                 startActivity(conec);
             }
         });
-
     }
 }
